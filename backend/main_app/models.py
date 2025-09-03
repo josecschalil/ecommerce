051f390a71af38642dd176c0dffa6402d3d0bad4
jose <josecschalil@gmail.com>
2025-09-04 00:03:47 +0530

    Before starting mass routing

--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -50,6 +50,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     original_price = models.DecimalField(max_digits=10, decimal_places=2)
